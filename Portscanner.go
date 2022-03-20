4dc70c1dacd23436d8bcae67998a6148a9d50d98
Danillo Perez <danillo0101@live.nl>
2022-03-20 21:36:57 +0100

    user input + URL-validation

--- Portscanner.go
+++ Portscanner.go
@@ -3,25 +3,38 @@ package main
 import (
 	"fmt"
 	"net"
+	"net/url"
+	"os"
+	"strconv"
 	"sync"
 )
 
 func main() {
+	address, err := ValidateUrl(os.Args[1])
+	if err != nil {
+		fmt.Printf(err.Error())
+		return
+	}
 	waitgroup := new(sync.WaitGroup)
 	waitgroup.Add(1024)
 	for i := 1; i <= 1024; i++ {
-		go PortScan(i, waitgroup)
+		go PortScan(i, waitgroup, address+":"+strconv.Itoa(i))
 	}
 	waitgroup.Wait()
 }
-func PortScan(j int, waitgroup *sync.WaitGroup) {
+func PortScan(port int, waitgroup *sync.WaitGroup, address string) {
 	defer waitgroup.Done()
-	address := fmt.Sprintf("scanme.nmap.org:%d", j)
-
 	_, err := net.Dial("tcp", address)
 	if err != nil {
-		fmt.Printf("error occured:%d\n", j)
+		fmt.Printf("error occured:%d\n", port)
 		return
 	}
-	fmt.Printf("Port Open: %d\n", j)
+	fmt.Printf("Port Open: %d\n", port)
+}
+func ValidateUrl(address string) (returnAddress string, returnerr error) {
+	_, err := url.Parse(address)
+	if err != nil {
+		return "", fmt.Errorf("invalid URL")
+	}
+	return address, nil
 }
